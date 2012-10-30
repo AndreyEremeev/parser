134652d5d79bdde153678576c4df1ecbd0d244b4
Andrew <elk24@i.ua>
2012-10-30 19:19:04 +0200

    parser2

---
+++ RSS-reader-sec/RSS reader sec/AppDelegate.m
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.m
+//  RSS reader sec
+//
+//  Created by admin on 10/27/12.
+//  Copyright (c) 2012 AndrewEremeev. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "RootViewController.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [_window release];
+    [super dealloc];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:[[RootViewController new]autorelease]];
+    self.window.rootViewController = navController;
+    [navController release];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+@end
